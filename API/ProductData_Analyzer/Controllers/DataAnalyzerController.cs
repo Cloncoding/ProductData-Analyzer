f846ec6f957f4b296e5bf593bbc02cf8f21eb495
Cloncoding <crystel2@gmx.de>
2024-08-12 18:47:24 +0200

    Added new API

---
+++ API/ProductData_Analyzer/Controllers/DataAnalyzerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductData_Analyzer.Controllers
+{
+    [ApiController]
+    [Route("analyzer/[controller]")]
+    public class DataAnalyzerController : ControllerBase
+    {
+
+        public DataAnalyzerController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "" };
+        }
+    }
+}

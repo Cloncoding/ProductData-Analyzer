bde802ef196e5f6a8256aa6a84c8706c2e50c793
Cloncoding <crystel2@gmx.de>
2024-08-13 03:06:42 +0200

    Added everything to API

--- API/ProductData_Analyzer/Controllers/DataAnalyzerController.cs
+++ API/ProductData_Analyzer/Controllers/DataAnalyzerController.cs
@@ -1,21 +1,191 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProductData_Analyzer.src;
+using System.Text.Json;
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using Microsoft.Extensions.Options;
 
 namespace ProductData_Analyzer.Controllers
 {
     [ApiController]
-    [Route("analyzer/[controller]")]
+    [Route("analyzer")]
     public class DataAnalyzerController : ControllerBase
     {
-
-        public DataAnalyzerController()
+        private JsonSerializerOptions options = new JsonSerializerOptions
         {
-        }
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+            WriteIndented = true
+        };
+
 
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<string> GetAsync(string? url, float? price, int? filter = -1)
         {
-            return new[] { "" };
+            if(url == null)
+            {
+                return "Please provide URL as query paremeter!";
+            }
+
+            List<ProductData> data = await GetFromUrl(url);
+
+
+            switch(filter)
+            {
+                case 1:
+                    {
+                        return GetMostExpensiveAndCheapest(data);
+                    }
+                case 2:
+                    {
+                        if(price == null)
+                        {
+                            return "Please provide price as query paremeter!";
+                        }
+
+                        return GetWithSpecificPrice(data, price);
+                    }
+                case 3:
+                    {
+                        return GetMostBottles(data);
+                    }
+                default:
+                    {
+                        if(price == null)
+                        {
+                            return "Please provide price as query paremeter!";
+                        }
+
+                        return GetAll(data, price);
+                    }
+            }
+
+            return filter + " " + url + " " + price;
+        }
+
+        private async Task<List<ProductData>> GetFromUrl(string url)
+        {
+            HttpClient client = new()
+            {
+                BaseAddress = new Uri(url)
+            };
+
+            HttpResponseMessage response = await client.GetAsync("");
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<List<ProductData>>(jsonResponse);
+        }
+
+
+        private string GetMostExpensiveAndCheapest(List<ProductData> data)
+        {
+            ProductData? mostExpensive = null, cheapest = null;
+            float mostExpensivePrice = 0, cheapestPrice = 0;
+
+            foreach(var product in data)
+            {
+                if(product.articles ==  null || product.articles.Length < 1)
+                {
+                    continue;
+                }
+
+                foreach(var article in product.articles)
+                {
+                    if(article.pricePerUnitText == null)
+                    {
+                        continue;
+                    }
+
+                    float ppl = ExtractPricePerLiter(article.pricePerUnitText);
+
+                    if(mostExpensive == null || ppl > mostExpensivePrice)
+                    {
+                        mostExpensivePrice = ppl;
+                        mostExpensive = new ProductData(product, article);
+                    }
+                    else if(cheapest == null || ppl < cheapestPrice)
+                    {
+                        cheapestPrice = ppl;
+                        cheapest = new ProductData(product, article);
+                    }
+                }
+            }
+
+            return JsonSerializer.Serialize(new ProductData[] { mostExpensive, cheapest }, options);
+        }
+
+        private float ExtractPricePerLiter(string text)
+        {
+            var match = Regex.Match(text.Replace(',', '.'), @"([-+]?[0-9]*\.?[0-9]+)");
+            return Convert.ToSingle(match.Groups[1].Value, CultureInfo.InvariantCulture);
+        }
+
+        private string GetWithSpecificPrice(List<ProductData> data, float? price)
+        {
+            List<ProductData> match = new List<ProductData>();
+
+            foreach(var product in data)
+            {
+                if(product.articles == null || product.articles.Length < 1)
+                {
+                    continue;
+                }
+
+                foreach(var article in product.articles)
+                {
+                    if(article.pricePerUnitText == null)
+                    {
+                        continue;
+                    }
+
+                    if(article.price.ToString().Equals(price.ToString()))
+                    {
+                        match.Add(new ProductData(product, article));
+                    }
+                }
+            }
+
+            return JsonSerializer.Serialize(match.OrderBy(p => ExtractPricePerLiter(p.articles[0].pricePerUnitText)).ToArray(), options);
+        }
+
+        private string GetMostBottles(List<ProductData> data)
+        {
+            ProductData? mostBottlesProduct = null;
+            int mostBottles = 0;
+
+            foreach(var product in data)
+            {
+                if(product != null && product.articles != null && product.articles.Length > 0)
+                {
+                    foreach(var article in product.articles)
+                    {
+                        if(article.shortDescription == null)
+                        {
+                            continue;
+                        }
+
+                        int bottles = Convert.ToInt32(article.shortDescription.Split('x')[0].Trim());
+
+                        if(bottles > mostBottles)
+                        {
+                            mostBottles = bottles;
+                            mostBottlesProduct = new ProductData(product, article);
+                        }
+                    }
+                }
+            }
+
+            return JsonSerializer.Serialize(mostBottlesProduct, options);
+        }
+
+        private string GetAll(List<ProductData> data, float? price)
+        {
+            return JsonSerializer.Serialize(
+                new List<string>() { GetMostExpensiveAndCheapest(data), GetWithSpecificPrice(data, price), GetMostBottles(data) },
+                options);
         }
     }
 }
---
+++ API/ProductData_Analyzer/src/ProductData.cs
@@ -0,0 +1,55 @@
+﻿namespace ProductData_Analyzer.src
+{
+    public class ProductData
+    {
+        public int id { get; set; }
+        public string? brandName { get; set; }
+        public string? name { get; set; }
+        public string? descriptionText { get; set; }
+        public Article[]? articles { get; set; }
+
+
+        public ProductData() { }
+
+
+        public ProductData(int id, string brandName, string name, string descriptionText, Article article)
+        {
+            this.id = id;
+            this.brandName = brandName;
+            this.name = name;
+            this.descriptionText = descriptionText;
+            articles = [article];
+        }
+
+        public ProductData(ProductData data, Article article)
+        {
+            this.id = data.id;
+            this.brandName = data.brandName;
+            this.name = data.name;
+            this.descriptionText = data.descriptionText;
+            articles = [article];
+        }
+
+
+        public class Article
+        {
+            public int id { get; set; }
+            public string? shortDescription { get; set; }
+            public float price { get; set; }
+            public string? unit { get; set; }
+            public string? pricePerUnitText { get; set; }
+            public string? image { get; set; }
+
+
+            public Article(int id, string shortDescription, float price, string unit, string pricePerUnitText, string image)
+            {
+                this.id = id;
+                this.shortDescription = shortDescription;
+                this.price = price;
+                this.unit = unit;
+                this.pricePerUnitText = pricePerUnitText;
+                this.image = image;
+            }
+        }
+    }
+}
